a11250795660aacc88c2dcca53c86acc6875eaec
Ricardo Ramirez <rikram101@arizona.edu>
2025-11-17 12:53:12 -0700

    added users model and route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+const User = require("./models/Users");
 
 // Telling express to use ejs as the templating engine
 app.set("view engine", "ejs");
@@ -10,6 +10,10 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/user", (req, res) => {
+  res.render("user");
 });
 
 app.listen(8080, () => {
